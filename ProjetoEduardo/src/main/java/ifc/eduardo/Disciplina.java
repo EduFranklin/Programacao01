1a495398ca000ec2dac333627c6402b3e59f6332
aluno <aluno@lab01-26>
2026-03-24 13:02:55 -0300

    dfxh

---
+++ ProjetoEduardo/src/main/java/ifc/eduardo/Disciplina.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifc.eduardo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Disciplina {
+
+    private int codDisciplina;
+    private String nomeDisciplina;
+    private int cargaHoraria;
+    private String ementa;
+    private String objDAprendizagem;
+    private String conteudosProgramaticos;
+    private String referenciasBibliograficas;
+
+    public int getCodDisciplina() {
+        return codDisciplina;
+    }
+
+    public void setCodDisciplina(int _codDisciplina) {
+        codDisciplina = _codDisciplina;
+    }
+
+    public String getNomeDisciplina() {
+        return nomeDisciplina;
+    }
+
+    public void setNomeDisciplina(String _nomeDisciplina) {
+        nomeDisciplina = _nomeDisciplina;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int _cargaHoraria) {
+        cargaHoraria = _cargaHoraria;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String _ementa) {
+        ementa = _ementa;
+    }
+
+    public String getObjDAprendizagem() {
+        return objDAprendizagem;
+    }
+
+    public void setObjDAprendizagem(String _objDAprendizagem) {
+        objDAprendizagem = _objDAprendizagem;
+    }
+
+    public String getConteudosProgramaticos() {
+        return conteudosProgramaticos;
+    }
+
+    public void setConteudosProgramaticos(String _conteudosProgramaticos) {
+        conteudosProgramaticos = _conteudosProgramaticos;
+    }
+
+    public String getReferenciasBibliograficas() {
+        return referenciasBibliograficas;
+    }
+
+    public void setReferenciasBibliograficas(String _referenciasBibliograficas) {
+        referenciasBibliograficas = _referenciasBibliograficas;
+    }
+}
