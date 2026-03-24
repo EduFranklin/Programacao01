9bb7e2e477fd8903425a7b0dcae4d7952e7c8424
aluno <aluno@lab01-26>
2026-03-24 12:59:56 -0300

    dzfh

--- ProjetoEduardo/src/main/java/ifc/eduardo/Professor.java
+++ ProjetoEduardo/src/main/java/ifc/eduardo/Professor.java
@@ -11,6 +11,7 @@ import java.util.Date;
  * @author aluno
  */
 public class Professor {
+
     private String nome;
     private int numeroDeIdentificacaoInstitucional;
     private String email;
@@ -19,69 +20,78 @@ public class Professor {
     private String areaDeAtuacao;
     private int regimeDeTrabalhoNaInstituicao;
     private Date dataDeIngresso;
-    
-    
-    public String getNome(){
+    private String formacao;
+
+    public String getNome() {
         return nome;
     }
-    public void setNome(String _nome){
+
+    public void setNome(String _nome) {
         nome = _nome;
     }
-    
-    
-    public int getnumeroDeIdentificacaoInstitucional(){
+
+    public int getnumeroDeIdentificacaoInstitucional() {
         return numeroDeIdentificacaoInstitucional;
     }
-    public void setNome(int _numeroDeIdentificacaoInstitucional){
+
+    public void setNome(int _numeroDeIdentificacaoInstitucional) {
         numeroDeIdentificacaoInstitucional = _numeroDeIdentificacaoInstitucional;
     }
-    
-    
-    public String getemail(){
+
+    public String getemail() {
         return email;
     }
-    public void setemail(String _email){
+
+    public void setemail(String _email) {
         email = _email;
     }
-    
-    
-    public int gettelefone(){
+
+    public int gettelefone() {
         return telefone;
     }
-    public void settelefone(int _telefone){
+
+    public void settelefone(int _telefone) {
         telefone = _telefone;
     }
-    
-    
-    public String gettitulacaoAcademica(){
+
+    public String gettitulacaoAcademica() {
         return titulacaoAcademica;
     }
-    public void settitulacaoAcademica(String _titulacaoAcademica){
+
+    public void settitulacaoAcademica(String _titulacaoAcademica) {
         titulacaoAcademica = _titulacaoAcademica;
     }
-    
-    
-    public String getareaDeAtuacao(){
+
+    public String getareaDeAtuacao() {
         return areaDeAtuacao;
     }
-    public void setareaDeAtuacao(String _areaDeAtuacao){
+
+    public void setareaDeAtuacao(String _areaDeAtuacao) {
         areaDeAtuacao = _areaDeAtuacao;
     }
-    
-    
-    public int getregimeDeTrabalhoNaInstituicao(){
+
+    public int getregimeDeTrabalhoNaInstituicao() {
         return regimeDeTrabalhoNaInstituicao;
     }
-    public void setregimeDeTrabalhoNaInstituicao(int _regimeDeTrabalhoNaInstituicao){
+
+    public void setregimeDeTrabalhoNaInstituicao(int _regimeDeTrabalhoNaInstituicao) {
         regimeDeTrabalhoNaInstituicao = _regimeDeTrabalhoNaInstituicao;
     }
-    
-    
-    public Date getdataDeIngresso(){
+
+    public Date getdataDeIngresso() {
         return dataDeIngresso;
     }
-    public void setdataDeIngresso(Date _dataDeIngresso){
+
+    public void setdataDeIngresso(Date _dataDeIngresso) {
         dataDeIngresso = _dataDeIngresso;
     }
-    
+
+    public String getformacao() {
+        return formacao;
+    }
+
+    public void setformacao(String _formacao) {
+        formacao = _formacao;
+    }
+
 }
